8129d395bd4f0cb22ee8c5a965ea52089f619edd
PasiSchukov <pasi.schukov@qalmari.fi>
2018-10-26 14:28:14 +0300

    brach muutokset1

--- testi.c
+++ testi.c
@@ -2,9 +2,14 @@ void main(void)
 {
     Uint8 u8_dummy = 0;
     
-    for( Uint8 i = 0; i < 10; i++)
+    for( Uint8 i = 0; i < 5; i++)
     {
         u8_dummy++;
+    }
+    
+    if( u8_dummy != 5 )
+    {
+        u8_dummy = 5;
     }
     
     return;
